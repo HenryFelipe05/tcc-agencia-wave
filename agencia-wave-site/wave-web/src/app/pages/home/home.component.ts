032b527abafe93b71a05198e84be710123c1612c
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-03-12 20:29:51 -0300

    feat: criando e estruturando projeto angular

---
+++ agencia-wave-site/wave-web/src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { TitleService } from '../../core/services/title.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true, 
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  /* Mostra o nome da página no title */
+  constructor(private titleService: TitleService) {
+    this.titleService.updateTitle('Home');
+  }
+}
