5e9505bd1fe98188f3d346c45094ba2f6f544478
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-03-13 17:00:48 -0300

    feat: Componentizando navbar e criando paginas de assinatura e galeria

--- agencia-wave-site/wave-web/src/app/app.component.ts
+++ agencia-wave-site/wave-web/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
-  imports: [CommonModule, RouterModule, RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- agencia-wave-site/wave-web/src/app/app.routes.ts
+++ agencia-wave-site/wave-web/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { GaleryComponent } from './pages/galery/galery.component';
+import { SubscriptionComponent } from './pages/subscription/subscription.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'contato', component: ContactComponent },
+    { path: 'galeria', component: GaleryComponent },
+    { path: 'assinaturas', component: SubscriptionComponent },
     { path: '**', redirectTo: '' }
 ];
