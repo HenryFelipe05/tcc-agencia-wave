00c857a59ab6a2614e76c96f01c72618b31cf395
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-03-18 22:27:22 -0300

    feat: adicionando estilios na página home

--- agencia-wave-site/wave-web/src/app/pages/home/home.component.ts
+++ agencia-wave-site/wave-web/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TitleService } from '../../core/services/title.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true, 
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
