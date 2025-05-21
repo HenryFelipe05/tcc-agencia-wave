be25fbfc353b76f36819fa9c8ae7d75ed1316c41
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-05-20 23:47:56 -0300

    feat: criando service da galeria.

--- agencia-wave-site/wave-api/Wave.Application/obj/Debug/net8.0/Wave.Application.AssemblyInfo.cs
+++ agencia-wave-site/wave-api/Wave.Application/obj/Debug/net8.0/Wave.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wave.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f97b7011953cb77d9fdd2400127b0e4ca68dd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c7172ce9fd964c3126607e1de2e6a05e880e19e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wave.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wave.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
