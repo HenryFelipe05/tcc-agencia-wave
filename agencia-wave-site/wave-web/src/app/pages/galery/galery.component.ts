170a830ed09af2c5bdc3645b39364ac275547525
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-06-29 20:57:20 -0300

    fix: ajustando metodo que retorna itens da galeira

--- agencia-wave-site/wave-web/src/app/pages/galery/galery.component.ts
+++ agencia-wave-site/wave-web/src/app/pages/galery/galery.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { TitleService } from '../../core/services/title.service';
 import { GaleryItemComponent } from '../../components/galery-item/galery-item.component';
 import { GaleryNavComponent } from '../../components/galery-nav/galery-nav.component';
+import { GaleriaService } from '../../services/galeria-service/galeria.service';
 
 @Component({
   selector: 'app-galery',
@@ -12,7 +13,18 @@ import { GaleryNavComponent } from '../../components/galery-nav/galery-nav.compo
   styleUrl: './galery.component.css'
 })
 export class GaleryComponent {
-  constructor(private titleService: TitleService) {
+  constructor(private titleService: TitleService, private galeriaService: GaleriaService) {
     this.titleService.updateTitle('Galeria');
+  }
+
+  ngOnInit(): void {
+    this.galeriaService.recuperarItensGaleria().subscribe({
+      next: (itens) => {
+        console.log('Itens da galeria:', itens);
+      },
+      error: (err) => {
+        console.error('Erro ao carregar galeria:', err);
+      }
+    });
   }
 }
--- agencia-wave-site/wave-web/src/app/services/galeria-service/galeria.service.ts
+++ agencia-wave-site/wave-web/src/app/services/galeria-service/galeria.service.ts
@@ -11,36 +11,7 @@ export class GaleriaService {
 
   constructor(private http: HttpClient) { }
 
-  criarItem(command: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/Criar`, command);
-  }
-
-  baixarItem(codigoItemGaleria: number): Observable<Blob> {
-    return this.http.get(`${this.apiUrl}/baixar/${codigoItemGaleria}`, {
-      responseType: 'blob'
-    });
-  }
-
-  buscarItens(queryParams: any): Observable<any[]> {
-    let params = new HttpParams();
-    for (let key in queryParams) {
-      if (queryParams[key] !== null && queryParams[key] !== undefined) {
-        params = params.set(key, queryParams[key]);
-      }
-    }
-    return this.http.get<any[]>(`${this.apiUrl}/Filtrar`, { params });
-  }
-
-  alterarItem(command: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/Alterar Item`, command);
-  }
-
-  excluirItem(codigoItemGaleria: number, codigoUsuario: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/Excluir Item`, {
-      params: {
-        codigoItemGaleria: codigoItemGaleria.toString(),
-        codigoUsuario: codigoUsuario.toString()
-      }
-    });
+  recuperarItensGaleria(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/recuperar-itens`);
   }
 }
